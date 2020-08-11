7f38033cd740aee923322d89120676a327dacb35
sblee1207 <sblee1207@naver.com>
2020-08-11 21:11:30 +0900

    Day-1
    command+s 하면 저장 된다 왕 신기

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Button, Progress} from 'semantic-ui-react'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <p> 안녕하 세 요 ! </p>
+    <Button Primary> hi </Button>
+    <Progress percent = {33} indicating/>
     </div>
   );
 }
