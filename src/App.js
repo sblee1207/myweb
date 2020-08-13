1edb8501dd31d6a5a3612428a5315747dc5ad5bf
sblee1207 <sblee1207@naver.com>
2020-08-13 19:55:14 +0900

    Day 2(1)
    로그인 버튼이 생겼어요!

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {Button, Progress} from 'semantic-ui-react'
+import {Button, Progress, Input} from 'semantic-ui-react'
+
+function Login(){
+
+  return(
+    <div>
+    <Input/>
+    <Button primary> 로그인! </Button>
+    </div>
+
+  )
+}
+
 
 function App() {
   return (
     <div>
-<p> Day 1 </p>
+<p> Day 2(1) </p>
 <Button Primary> help </Button>
-<Progress percent = {66} indicating/>
+<Progress percent = {10} indicating/>
+<Login / >
 </div>
   );
 }
